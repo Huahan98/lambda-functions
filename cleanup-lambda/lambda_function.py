44548633b9e589f9212cc2b82a4fed15d2b123e7
Qu <huahq@MacBook-Pro.local>
2020-06-23 23:49:33 -0400

    working version of cleanup lambda

--- cleanup-lambda/lambda_function.py
+++ cleanup-lambda/lambda_function.py
@@ -1,10 +1,29 @@
-import json
+from datetime import datetime
+from dateutil.tz import tzlocal
+
 import boto3
+
+CLEANUP_THRESHOLD_IN_SECONDS = 86400 # 24 hours
+BUCKET_NAME = "dlc-test-tickets"
+FOLDER_NAME = "resource_pool/"
+
 
 def lambda_handler(event, context):
     s3_client = boto3.client("s3")
-    response = s3_client.list_objects(Bucket="dlc-test-tickets", MaxKeys=1,
-                           Prefix=f"resource_pool/")
-    print(response)
+    list_objects_response = s3_client.list_objects(Bucket=BUCKET_NAME, Prefix=FOLDER_NAME)
 
-    return
+    # Scan for outdated resource pool tickets
+    deletion_list = []
+    for file in list_objects_response["Contents"]:
+        if file["Key"].endswith(".json"):
+            last_modified_time = file["LastModified"]
+            total_seconds_passed = (datetime.now(tzlocal()) - last_modified_time).total_seconds()
+            if total_seconds_passed >= CLEANUP_THRESHOLD_IN_SECONDS:
+                    deletion_list.append({"Key": file["Key"]})
+
+    # Delete outdated resource pool tickets
+    if len(deletion_list) != 0:
+        deletion_response = s3_client.delete_objects(Bucket=BUCKET_NAME, Delete={"Objects": deletion_list, "Quiet":False})
+        print(deletion_response)
+
+    print(f"Deleted {str(len(deletion_list))} resource pool tickets.")
